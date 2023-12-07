93e359b1498a445b4975872957dd5ce6d3cb0f04
saralune <sarahlef59@hotmail.fr>
2023-12-07 22:45:07 +0100

    add form

--- game_clicker/lib/game.dart
+++ game_clicker/lib/game.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -13,7 +15,9 @@ class _GameState extends State<Game> {
   var _clickCount = 0;
   var _displayScoreBtn = false;
   int _record = 0;
+  var _recordman = "";
   var _prenom = "";
+  var _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +28,7 @@ class _GameState extends State<Game> {
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Text('Voici ton score : $_clickCount'),
               if (_displayScoreBtn)
@@ -33,21 +37,46 @@ class _GameState extends State<Game> {
                     icon: Icon(Icons.plus_one_sharp),
                     //color: Color(Colors.limeAccent),
                     label: Text(' Go Go Go !')),
-              //Spacer(),
-              if (_record > 0) Text("Voici le record absolu : $_record"),
-              // Spacer(),
-              Text("Ton prénom : $_prenom"),
-              TextField(
-                autocorrect: false, //pas besoin pour le prénom
-                textCapitalization: TextCapitalization
-                    .words, //utile pour avoir un prénom avec une majuscule //désactiver la majuscule pour le mail
-                autofillHints: [AutofillHints.givenName],
-                keyboardType: TextInputType
-                    .name, //assiste l'utilisateur, son tel lui proposera un prénom de contact
-                //onChanged: _modifiedName, //en temps réel !
-                onSubmitted: _modifiedName,
+              if (_record > 0)
+                Text(
+                    "Voici le record absolu : $_record - réalisé par $_recordman"),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  if (!_displayScoreBtn) Text("Ton prénom : $_prenom"),
+                  if (!_displayScoreBtn)
+                    Form(
+                      key: _formKey,
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.person_2_rounded),
+                          Expanded(
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  helperText: "Entrez votre prénom",
+                                  hintText: "Prénom"),
+                              autocorrect: false, //pas besoin pour le prénom
+                              textCapitalization: TextCapitalization
+                                  .words, //utile pour avoir un prénom avec une majuscule //désactiver la majuscule pour le mail
+                              autofillHints: [AutofillHints.givenName],
+                              keyboardType: TextInputType
+                                  .name, //assiste l'utilisateur, son tel lui proposera un prénom de contact
+                              //onChanged: _saveFirstName, //en temps réel !
+                              onSaved: _saveFirstName,
+                              validator: (value) => value!.length > 2
+                                  ? null
+                                  : "Prénom trop court !",
+                            ),
+                          ),
+                          IconButton(
+                              onPressed: _confirmFirstName,
+                              icon: Icon(Icons.check_circle_outlined)),
+                        ],
+                      ),
+                    ),
+                ],
               ),
-              if (!_displayScoreBtn)
+              if (!_displayScoreBtn && _prenom != "")
                 FloatingActionButton.extended(
                     onPressed: _toggleScoreBtn,
                     label: Text("Commencer à scorer !")),
@@ -56,7 +85,7 @@ class _GameState extends State<Game> {
         ));
   }
 
-  _modifiedName(value) {
+  _saveFirstName(value) {
     setState(() {
       _prenom = value;
     });
@@ -73,7 +102,10 @@ class _GameState extends State<Game> {
   _stopGame() {
     setState(() {
       _displayScoreBtn = !_displayScoreBtn;
-      if (_clickCount > _record) _record = _clickCount;
+      if (_clickCount > _record) {
+        _record = _clickCount;
+        _recordman = _prenom;
+      }
     });
   }
 
@@ -81,5 +113,11 @@ class _GameState extends State<Game> {
     setState(() {
       _clickCount++;
     });
+  }
+
+  _confirmFirstName() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    }
   }
 }
