798ae7e76fdcd9f194abb7dea5f3c13c16dd212d
saralune <sarahlef59@hotmail.fr>
2023-12-08 22:46:57 +0100

    add stateful widget for picture import

---
+++ lists_project/lib/list_learning.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListLearning extends StatefulWidget {
+  const ListLearning({super.key});
+
+  @override
+  State<ListLearning> createState() => _ListLearningState();
+}
+
+class _ListLearningState extends State<ListLearning> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
