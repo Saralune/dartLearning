cc960e222fd820015964d836a72bde5ca5c1e3ff
saralune <sarahlef59@hotmail.fr>
2024-01-11 17:03:21 +0100

    update login screen

--- flutter_archi/lib/ui/screens/login_screen.dart
+++ flutter_archi/lib/ui/screens/login_screen.dart
@@ -6,50 +6,77 @@ class LoginScreen extends StatelessWidget {
   final LoginViewModel _viewModel;
   const LoginScreen(this._viewModel, {Key? key}) : super(key: key);
 
+  _loggedInButtonPressed() {
+    _viewModel.loginUser();
+  }
+
+  _isPasswordOK(value) {
+    if (value == null || value.isEmpty || !value.contains("ok")) {
+      return "Le mot de passe n'est pas correct !";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final errorMessage = _viewModel.errorMessage;
-
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: const [
-                  AutofillHints.username,
-                  AutofillHints.email
-                ],
-                decoration: const InputDecoration(labelText: "Email"),
-              ),
-              TextFormField(
-                obscureText: true,
-                autofillHints: const [AutofillHints.password],
-                decoration: const InputDecoration(labelText: "Mot de passe"),
-              ),
-              const SizedBox(height: 8),
-              if (_viewModel.isLoading)
-                const Center(
-                  child: CircularProgressIndicator(),
-                )
-              else
-                Center(
-                  child: ElevatedButton(
-                      onPressed: _loggedInButtonPressed,
-                      child: const Text("Se connecter")),
-                ),
-              if (errorMessage != null)
-                Text(
-                  errorMessage,
-                  style: const TextStyle(
-                      color: Colors.redAccent, fontWeight: FontWeight.bold),
-                )
-            ],
-          )),
+          child: AnimatedBuilder(
+              animation: _viewModel,
+              builder: (context, child) {
+                final errorMessage = _viewModel.errorMessage;
+                return Column(
+                  children: [
+                    TextFormField(
+                      onChanged: _viewModel.emailChanged,
+                      keyboardType: TextInputType.emailAddress,
+                      autofillHints: const [
+                        AutofillHints.username,
+                        AutofillHints.email
+                      ],
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          errorText: _viewModel.emailErrorMessage),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            !value.contains("ok")) {
+                          return "Le mot de passe n'est pas correct !";
+                        }
+                        return null;
+                      },
+                      onChanged: _viewModel.passwordChanged,
+                      obscureText: true,
+                      autofillHints: const [AutofillHints.password],
+                      decoration: InputDecoration(
+                          labelText: "Mot de passe",
+                          errorText: _viewModel.passwordErrorMessage),
+                    ),
+                    const SizedBox(height: 8),
+                    if (_viewModel.isLoading)
+                      const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    else
+                      Center(
+                        child: ElevatedButton(
+                            onPressed: _loggedInButtonPressed,
+                            child: const Text("Se connecter")),
+                      ),
+                    if (errorMessage != null)
+                      Text(
+                        errorMessage,
+                        style: const TextStyle(
+                            color: Colors.redAccent,
+                            fontWeight: FontWeight.bold),
+                      )
+                  ],
+                );
+              })),
     );
   }
-
-  void _loggedInButtonPressed() {}
 }
--- flutter_archi/lib/ui/screens/login_viewmodel.dart
+++ flutter_archi/lib/ui/screens/login_viewmodel.dart
@@ -1,7 +1,61 @@
-class LoginViewModel {
+import 'package:flutter/material.dart';
+
+class LoginViewModel extends ChangeNotifier {
   bool _isLoading = false;
-  String? _errorMessage = "c'est du caca d'erreur !";
+  String? _emailErrorMessage;
+  String? _passwordErrorMessage;
+  String? _errorMessage;
+
+  String? get passwordErrorMessage => _passwordErrorMessage;
+  String? get emailErrorMessage => _emailErrorMessage;
+  String? _email;
+  String? _password;
 
   bool get isLoading => _isLoading;
   String? get errorMessage => _errorMessage;
+
+  bool get _minimalInputIsValid =>
+      _email != null &&
+      _password != null &&
+      _emailErrorMessage == null &&
+      _passwordErrorMessage == null;
+
+  void loginUser() async {
+    final email = _email;
+    final password = _password;
+
+    if (email != null && password != null) {
+      if (email.contains("@") == false) {
+        _emailErrorMessage = "L'email ne contient pas d' \"@\"";
+        notifyListeners();
+      }
+      if (password.isEmpty) {
+        _passwordErrorMessage = "Le mot de passe ne doit pas être vide.";
+        notifyListeners();
+      }
+
+      if (_minimalInputIsValid) {
+        _isLoading = true;
+        notifyListeners();
+        await Future.delayed(const Duration(seconds: 2));
+        _isLoading = false;
+        //_errorMessage = "Login attempt with $email and $password";
+        _errorMessage = password == "ok"
+            ? "Bienvenue $email !"
+            : "Impossible de retrouver votre compte...";
+        notifyListeners();
+      }
+    }
+    notifyListeners();
+  }
+
+  void emailChanged(String newEmail) {
+    _email = newEmail;
+    _emailErrorMessage = null;
+  }
+
+  void passwordChanged(String newPassword) {
+    _password = newPassword;
+    _passwordErrorMessage = null;
+  }
 }
