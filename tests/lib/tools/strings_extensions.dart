14e451ef487bb340569eeab8c816a39440e668d6
saralune <sarahlef59@hotmail.fr>
2024-01-19 18:10:45 +0100

    add email screen file + init main

---
+++ tests/lib/tools/strings_extensions.dart
@@ -0,0 +1,7 @@
+extension StringsExtensions on String {
+  bool get isValidEmail {
+    return RegExp(
+            r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+        .hasMatch(this);
+  }
+}
---
+++ tests/lib/ui/send_email_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:tests/tools/strings_extensions.dart';
+
+class SendEmailScreen extends StatefulWidget {
+  const SendEmailScreen({Key? key}) : super(key: key);
+
+  @override
+  _SendEmailScreenState createState() => _SendEmailScreenState();
+}
+
+class _SendEmailScreenState extends State<SendEmailScreen> {
+  String _message = "";
+  String _email = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Column(children: [
+        TextFormField(
+          key: const Key("email-field"),
+          decoration: const InputDecoration(
+            icon: Icon(Icons.email),
+            labelText: 'Votre adresse email',
+            helperText: 'Entrez votre adresse pour recevoir un message',
+          ),
+          onChanged: (value) => _email = value,
+        ),
+        ElevatedButton(
+            onPressed: _sendButtonTouched, child: const Text("Envoyer")),
+        Text(_message)
+      ]),
+    ));
+  }
+
+  _sendButtonTouched() {
+    setState(() {
+      if (_email.isValidEmail) {
+        _message = "Message envoyé à $_email";
+      } else {
+        _message = "Email invalide";
+      }
+    });
+  }
+}
